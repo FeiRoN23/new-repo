d93e3574875085a7be2b2c430bee0f0db59b6d35
Nabil Benhammou <benhammou.nabil@live.com>
2023-12-10 17:51:03 +0100

    test: experimenting with a simple test creating class and header in other project library

---
+++ cl_dll/hello.cpp
@@ -0,0 +1,6 @@
+#include "hello.h"
+
+int hello::doHello()
+{
+	return 4;
+}
--- projects/HLClientUnitTest/Test.cpp
+++ projects/HLClientUnitTest/Test.cpp
@@ -1,8 +1,18 @@
 #define BOOST_TEST_MODULE mytests
 #include <boost/test/included/unit_test.hpp>
+#include "hello.h"
+
 
 BOOST_AUTO_TEST_CASE(myTestCase)
 {
   BOOST_TEST(1 == 1);
   BOOST_TEST(true);
+}
+
+
+BOOST_AUTO_TEST_CASE(otherTest) {
+    
+	hello hello;
+
+	BOOST_CHECK_EQUAL(4, hello.doHello());
 }
