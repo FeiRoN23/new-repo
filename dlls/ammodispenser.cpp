278e684b74180638fb2488170cebd39565e46f3d
Nabil Benhammou <benhammou.nabil@live.com>
2024-01-08 20:34:59 +0100

    fix: finally fixed solid and move type to be overidden after ammo spawn

--- dlls/ammodispenser.cpp
+++ dlls/ammodispenser.cpp
@@ -4,12 +4,33 @@ LINK_ENTITY_TO_CLASS(ammo_dispenser, CAmmoDispenser)
 
 void CAmmoDispenser::Spawn()
 {
+    pev->solid = SOLID_NOT;
+    pev->movetype = MOVETYPE_NONE;
     ALERT(at_console, "Ammo dispenser spawned\n");
+    //UTIL_SetSize(pev, Vector(0, 0, 0), Vector(0, 0, 0));
 }
 
 void CAmmoDispenser::dispense_ammo() {
     ALERT(at_console, "dispensing 9mm ammo\n");
-    Create("ammo_9mmclip", pev->origin, pev->angles, edict());
+    //Create("item_sodacan", pev->origin, pev->angles, edict());
+    edict_t* pent;
+    pent = CREATE_NAMED_ENTITY(MAKE_STRING("ammo_9mmclip"));
+    if (FNullEnt(pent))
+    {
+        ALERT(at_console, "NULL Ent in Create!\n");
+        return NULL;
+    }
+    CBaseEntity* ammoEntity = Instance(pent);
+    
+    ammoEntity->pev->origin = pev->origin;
+    ammoEntity->pev->angles = pev->angles;
+    ammoEntity->Spawn();
+    ammoEntity->pev->movetype = MOVETYPE_NONE;
+    ammoEntity->pev->solid = SOLID_TRIGGER;
+
+    //Old method to spawn
+    //Create("ammo_9mmclip", pev->origin, pev->angles, edict()); 
+    
 }
 
 void CAmmoDispenser::Use(CBaseEntity* pActivator, CBaseEntity* pCaller, USE_TYPE useType, float value)
